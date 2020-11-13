ba27fe9de243a59a09cfa0f03d62ffb8b94ca7c7
levantuanpxu <70925631+levantuanpxu@users.noreply.github.com>
2020-11-13 12:02:22 +0700

    alo

---
+++ QuanLySach/QuanLySach/ViewsModels/BooksCreateVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuanLySach.Models;
+
+namespace QuanLySach.ViewsModels
+{
+    public class BooksCreateVM
+    {
+        public Books Books { get; set; }
+        public IEnumerable<SelectListItem> CategorySelectList { get; set; }
+    }
+}
